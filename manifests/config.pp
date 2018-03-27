7eaedfbf1626ad95039b649531feb2255643e388
Alessandra Forti <aforti@vm57.in.tier2.hep.manchester.ac.uk>
2018-03-27 16:40:20 +0100

    Added policies and intervals

--- manifests/config.pp
+++ manifests/config.pp
@@ -2,6 +2,7 @@
 # Sets up the configuration file and file dependencies.
 class argus_server::config {
 
+  # ARGUS configuration files
   file { '/etc/argus/pap/pap-admin.properties':
     ensure  => 'present',
     owner   => 'root',
@@ -37,4 +38,28 @@ class argus_server::config {
     content => template("${module_name}/pepd.ini.erb"),
     notify  => Service['argus-pepd'],
   }    
+
+  # XML policy to load
+  file { '/etc/argus/argus_policy.xml':
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0600',
+    content => template("${module_name}/argus_policy.erb"),
+  }
+  # Cleanup the policy before loading it
+  exec { 'remove_argus_policy':
+    command     => '/usr/bin/pap-admin remove-all-policies',
+    refreshonly => !$force_reload,
+    logoutput   => true, #"on_failure", # "true"
+    subscribe   => File['/etc/argus/argus_policy.xml'],
+  }
+
+  # Load the policy
+  exec { 'apply_argus_policy':
+    command     => '/usr/bin/pap-admin add-policies-from-file /etc/argus/argus_policy.xml',
+    refreshonly => true,
+    logoutput   => true, #"on_failure", # "true"
+    subscribe   => Exec['remove_argus_policy'],
+  }
 }
--- manifests/init.pp
+++ manifests/init.pp
@@ -11,17 +11,19 @@
 # Sample Usage:
 #
 class argus_server (
+  $packages = ['java-1.8.0-openjdk', 'argus-authz', 'bdii', 'glite-info-provider-service'],
   $argus_host = '',
   $argus_host_dn = '',
+  $pap_poll_interval = 3600,
+  $pdp_retention_interval = 240,  
+  $pap_policy = {},
   $centralban_enabled = 'false',
   $centralban_host = 'argusngi.gridpp.rl.ac.uk',
   $centralban_dn = '/C=UK/O=eScience/OU=CLRC/L=RAL/CN=argusngi.gridpp.rl.ac.uk',
-  $packages = ['java-1.8.0-openjdk', 'argus-authz', 'bdii', 'glite-info-provider-service'],
-  
 ){
   
   class { 'argus_server::install': }
   class { 'argus_server::config': }
   class { 'argus_server::services': }
-
+  Class['argus_server::install'] -> Class['argus_server::config'] -> Class['argus_server::services']
 }
