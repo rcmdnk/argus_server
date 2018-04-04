b5d27457ff9973bee59fab67998228543bc76fd7
Alessandra Forti <aforti@vm57.in.tier2.hep.manchester.ac.uk>
2018-04-04 15:11:29 +0100

    Fixed Policy uploaded sequence

--- manifests/policy.pp
+++ manifests/policy.pp
@@ -1,11 +1,5 @@
 class argus_server::policy {
 
-  # Restart argus if needed
-  exec {'restart argus':
-    command => '/usr/bin/systemctl restart argus-pap argus-pdp argus-pepd',
-    onlyif  => '/usr/bin/pap-admin lp | grep -q SSL',
-  }
-
   # XML policy to load
   file { '/etc/argus/argus_policy.xml':
     ensure  => 'present',
@@ -13,15 +7,29 @@ class argus_server::policy {
     group   => 'root',
     mode    => '0600',
     content => template("${module_name}/argus_policy.erb"),
-    require => Exec['restart argus']
+  }
+
+  # Restart argus if needed
+  exec {'ssl restart':
+    command => '/usr/bin/systemctl restart argus-pap argus-pdp argus-pepd',
+    onlyif  => '/usr/bin/pap-admin lp | grep -q SSL',
   }
 
   # Load the policy
   exec { 'apply argus policy':
+    command     => '/usr/bin/sleep 60; /usr/bin/pap-admin add-policies-from-file /etc/argus/argus_policy.xml;/usr/bin/systemctl restart argus-pap argus-pdp argus-pepd',
+    onlyif      => "/usr/bin/bash -c \"[[ -r /etc/argus/argus_policy.xml ]] && /usr/bin/pap-admin lp | grep -q 'No policies'\"",
+    logoutput   => true, #"on_failure", # "true"
+    require     => Exec['ssl restart']
+  }
+
+  # Load the policy
+  exec { 'refresh argus policy':
     command     => '/usr/bin/pap-admin add-policies-from-file /etc/argus/argus_policy.xml',
     refreshonly => true,
     logoutput   => true, #"on_failure", # "true"
     subscribe   => File['/etc/argus/argus_policy.xml'],
-    require     => Exec['restart argus'],  
   }
+        
+
 }              
