3ebaa556d449dc005cf978331b27b9a0f5a1bfb9
Alessandra Forti <aforti@vm57.in.tier2.hep.manchester.ac.uk>
2018-06-01 15:12:23 +0100

    Adding path to the exec commands in policy

--- manifests/policy.pp
+++ manifests/policy.pp
@@ -1,5 +1,8 @@
 class argus_server::policy {
 
+  $restart_cmd='service argus-pap restart; service argus-pdp restart; service argus-pepd restart'
+
+  notify { "$restart_cmd":}
   # XML policy to load
   file { '/etc/argus/argus_policy.xml':
     ensure  => 'present',
@@ -11,21 +14,24 @@ class argus_server::policy {
 
   # Restart argus if needed
   exec {'ssl restart':
-    command => '/usr/bin/systemctl restart argus-pap argus-pdp argus-pepd',
-    onlyif  => '/usr/bin/pap-admin lp | grep -q SSL',
+    path => '/usr/bin:/bin:/usr/sbin:/sbin',
+    command => $restat_cmd,
+    onlyif  => 'pap-admin lp | grep -q SSL',
   }
 
   # Load the policy
   exec { 'apply argus policy':
-    command     => '/usr/bin/sleep 60; /usr/bin/pap-admin add-policies-from-file /etc/argus/argus_policy.xml;/usr/bin/systemctl restart argus-pap argus-pdp argus-pepd',
-    onlyif      => "/usr/bin/bash -c \"[[ -r /etc/argus/argus_policy.xml ]] && /usr/bin/pap-admin lp | grep -q 'No policies'\"",
+    path => '/usr/bin:/bin:/usr/sbin:/sbin',
+    command     => "sleep 60; pap-admin add-policies-from-file /etc/argus/argus_policy.xml; $restart_cmd",
+    onlyif      => "bash -c \"[[ -r /etc/argus/argus_policy.xml ]] && pap-admin lp | grep -q 'No policies'\"",
     logoutput   => true, #"on_failure", # "true"
     require     => Exec['ssl restart']
   }
 
   # Load the policy
   exec { 'refresh argus policy':
-    command     => '/usr/bin/pap-admin add-policies-from-file /etc/argus/argus_policy.xml',
+    path => '/usr/bin:/bin:/usr/sbin:/sbin',
+    command     => 'pap-admin add-policies-from-file /etc/argus/argus_policy.xml',
     refreshonly => true,
     logoutput   => true, #"on_failure", # "true"
     subscribe   => File['/etc/argus/argus_policy.xml'],
