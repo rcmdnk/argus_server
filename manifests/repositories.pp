bc62f9c13a566f9fd6defddee4c99b50aacd5aa2
Michiru Kaneda <Michiru.Kaneda@gmail.com>
2018-10-16 23:03:36 +0900

    removed wlcg repo

--- manifests/repositories.pp
+++ manifests/repositories.pp
@@ -10,15 +10,6 @@ class argus_server::repositories {
           $umd_source = 'http://repository.egi.eu/sw/production/umd/3/sl6/x86_64/updates/umd-release-3.14.4-1.el6.noarch.rpm'
         }
         default: {
-          package { 'wlcg-repo':
-            ensure => installed,
-            source => "http://linuxsoft.cern.ch/wlcg/centos7/x86_64/wlcg-repo-1.0.0-1.el7.noarch.rpm",
-            provider => rpm,
-          }
-          package { "HEP_OSlibs":
-            ensure => installed,
-            require => Package['wlcg-repo'],
-          }
           $umd_source = 'http://repository.egi.eu/sw/production/umd/4/centos7/x86_64/updates/umd-release-4.1.3-1.el7.centos.noarch.rpm'
         }
       }
